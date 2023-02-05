559d2a32869cc62ec2830e33fa5ad3b1e1bb5556
MrBeanDev <34623886+mrbeandev@users.noreply.github.com>
2023-02-05 15:45:51 +0530

    Update tran_sql.py
    added progress bar

--- tran_sql.py
+++ tran_sql.py
@@ -2,6 +2,7 @@
 import re
 from sys import argv
 from googletrans import Translator
+from tqdm import tqdm
 
 def translate_sql(file_path):
     # Initialize the translator
@@ -10,14 +11,13 @@ def translate_sql(file_path):
     # Open the SQL file
     with open(file_path, 'r', encoding='utf-8-sig') as f:
         sql_text = f.read()
-    cou = 0
+
     # find all the chinese text and translate
-    for match in re.finditer(u'[\u4e00-\u9fff]+', sql_text):
+    matches = re.finditer(u'[\u4e00-\u9fff]+', sql_text)
+    for match in tqdm(matches, desc='Translating', total=len(sql_text)):
         chinese_text = match.group()
         translated_text = translator.translate(chinese_text, dest='en').text
         translated_text = translated_text.replace("'", "")
-        cou += 1
-        print(str(cou) + " - " + chinese_text + " = " +translated_text)
         sql_text = sql_text.replace(chinese_text, translated_text, 1)
 
     # Save the translated SQL file
