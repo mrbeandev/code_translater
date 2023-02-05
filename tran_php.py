753d02a82c30df782d61ef385511e0bacfd28502
MrBeanDev <34623886+mrbeandev@users.noreply.github.com>
2023-02-05 15:44:49 +0530

    Update tran_php.py
    added progress bar

--- tran_php.py
+++ tran_php.py
@@ -11,13 +11,11 @@ def translate_php(file_path):
     with open(file_path, 'r', encoding='utf-8-sig') as f:
         php_text = f.read()
     
-    cou = 0
     # find all the chinese text and translate
-    for match in re.finditer(u'[\u4e00-\u9fff]+', php_text):
+    matches = re.finditer(u'[\u4e00-\u9fff]+', php_text)
+    for match in tqdm(matches, desc='Translating', total=len(php_text)):
         chinese_text = match.group()
         translated_text = translator.translate(chinese_text, dest='en').text
-        cou += 1
-        print(str(cou)+" - "+chinese_text+" = "+translated_text)
         php_text = php_text.replace(chinese_text, translated_text, 1)
 
     # Save the translated PHP file
